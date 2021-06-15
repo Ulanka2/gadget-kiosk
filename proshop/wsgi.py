cc19e806ea323a848093ecff6fcd82c641f75d05
Nzekwe Isaac Edidiong <curlyzik@gmail.com>
2021-06-15 02:15:45 +0100

    Served staticfiles

--- proshop/wsgi.py
+++ proshop/wsgi.py
@@ -15,3 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proshop.settings.production')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, 'staticfiles')
+application.add_files('media', prefix='media/')
