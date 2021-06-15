d6a02d690fbb02e3e4b1b461282b93338f5443ec
Nzekwe Isaac Edidiong <curlyzik@gmail.com>
2021-06-15 02:33:48 +0100

    Added media

--- proshop/urls.py
+++ proshop/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('api/users/', include('core.urls.user_urls')),
     path('api/orders/', include('core.urls.order_urls')),
     path('accounts/', include('allauth.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     import debug_toolbar
